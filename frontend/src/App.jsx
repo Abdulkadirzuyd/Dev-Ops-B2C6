47b02b4bd5f498953cbfce36e73b89c20356af9c
OwenC1 <66129237+Fxreball@users.noreply.github.com>
2025-05-16 13:55:20 +0200

    front-end update

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -9,7 +9,7 @@ function App() {
 
     // Zodra de app wordt geladen, haal het bericht op uit Flask
     useEffect(() => {
-        fetch('172.201.187.117:5000/api/hello')
+        fetch('http://172.201.187.117:5000/api/hello')
             .then((res) => res.json())
             .then((data) => setMessage(data.message))
             .catch((err) => console.error('Fout bij ophalen API:', err))
