11b2406a910f8ca36854e60db2629e92e2ecdca6
HartyyICT <2104872asep@zuyd.nl>
2025-06-03 11:51:55 +0200

    Zuyd Homestyling toegepast en voorraad beheer af

--- frontend/src/pages/Storage/StoragePage.jsx
+++ frontend/src/pages/Storage/StoragePage.jsx
@@ -1,27 +1,24 @@
 import React from "react";
 import styles from "./StorageStyle.module.css";
-import Sidebar from "../SideBar/SideBar";
+
 
 const StoragePage = () => {
   return (
     <div className={styles.storageContainer}>
-      <div className={styles.sidebar}>
-        <Sidebar />
-      </div>
 
       <div className="content">
         <div className="topbar">
-          <button className="circle-btn">🔍</button>
-          <div className="title">Voorraadbeheer</div>
+          <h1 className="title">Voorraadbeheer</h1>
           <input className="small-input" />
           <input className="small-input" />
         </div>
 
         <div className="controls">
-          <button>Zoeken</button>
-          <div className="color-box"></div>
-          <button>Sorteren</button>
+            <button className={styles.button}>Zoeken</button>
+            <div className="color-box"></div>
+            <button className={styles.button}>Sorteren</button>
         </div>
+
 
         <div className="table">
           <div className="table-header">
