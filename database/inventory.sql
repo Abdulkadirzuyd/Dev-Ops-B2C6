43ed2471f7cf80df5932e51437cc6d46df3c9069
Alexander2104292 <94622134+Alexander2104292@users.noreply.github.com>
2025-06-26 11:51:56 +0200

    Update inventory.sql
    ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;

--- database/inventory.sql
+++ database/inventory.sql
@@ -28,7 +28,7 @@ CREATE TABLE `inventory` (
   `quantity_in_stock` int DEFAULT NULL,
   `minimum_required` int DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
