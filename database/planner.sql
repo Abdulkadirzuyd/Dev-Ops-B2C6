72f7c09da24b809e677cac94f75a70d3f12579ae
Alexander2104292 <94622134+Alexander2104292@users.noreply.github.com>
2025-06-26 11:52:26 +0200

    Update planner.sql
    ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;

--- database/planner.sql
+++ database/planner.sql
@@ -28,7 +28,7 @@ CREATE TABLE `planners` (
   `email` varchar(255) DEFAULT NULL,
   `phone_number` varchar(50) DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
